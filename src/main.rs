bf2ac4d62acef3d09d3800052e4c4a46f898c681
Josh Burns <joshyburnss@gmail.com>
2023-06-16 13:56:48 +1000

    imply quiet value when not provided

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,6 @@
-use std::collections::HashMap;
-use std::fs;
 use std::path::Path;
 
-use toml::Value;
-
 mod task;
-
-use task::Task;
-
-fn parse_taskfile(file_path: &str) -> HashMap<String, Task> {
-    // Read the TOML file
-    let toml_content = fs::read_to_string(file_path).expect("Failed to read file.");
-
-    // Parse the TOML content
-    let parsed_toml: Value = toml::from_str(&toml_content).expect("Failed to parse TOML.");
-
-    // Create a HashMap to store the tasks
-    let mut tasks: HashMap<String, Task> = HashMap::new();
-
-    // Iterate over the TOML table entries
-    if let Value::Table(table) = parsed_toml {
-        for (task_name, task_value) in table {
-            // Parse command string
-            let command = task_value["command"]
-                .as_str()
-                .expect("Invalid command.")
-                .to_string();
-
-            // Parse quiet settings true/false (default to false)
-            let quiet = task_value["quiet"]
-                .as_bool()
-                .unwrap_or(false); // Assigns false as default if not provided
-
-            // Build task and add to hashmap
-            let task = Task { command, quiet };
-            tasks.insert(task_name.to_string(), task);
-        }
-    }
-
-    tasks
-}
 
 fn main() {
     let file_path = "tasks.toml";
@@ -50,7 +11,7 @@ fn main() {
         return;
     }
 
-    let tasks = parse_taskfile(file_path);
+    let tasks = task::parse_taskfile(file_path);
 
     // Print the tasks
     for (task_name, task) in tasks {
--- src/task.rs
+++ src/task.rs
@@ -1,3 +1,9 @@
+use std::io;
+use std::fs;
+use std::io::Write;
+use std::collections::HashMap;
+use std::process::Command;
+
 #[derive(Debug)]
 pub struct Task {
     pub command: String,
@@ -17,4 +23,50 @@ impl Task {
             io::stderr().write_all(&output.stderr).unwrap();
         }
     }
+}
+
+pub fn parse_taskfile(file_path: &str) -> HashMap<String, Task> {
+    // Read the TOML file
+    let toml_content = fs::read_to_string(file_path).expect("Failed to read file.");
+
+    // Parse the TOML content
+    let parsed_toml: toml::Value = toml::from_str(&toml_content).expect("Failed to parse TOML.");
+
+    // Extract tasks from the TOML table
+    let tasks = extract_tasks(&parsed_toml);
+
+    tasks
+}
+
+fn extract_tasks(parsed_toml: &toml::Value) -> HashMap<String, Task> {
+    // Create a HashMap to store the tasks
+    let mut tasks: HashMap<String, Task> = HashMap::new();
+
+    // Iterate over the TOML table entries
+    if let toml::Value::Table(table) = parsed_toml {
+        for (task_name, task_value) in table {
+            // Parse command string
+            let command = task_value["command"]
+                .as_str()
+                .expect("Invalid command.")
+                .to_string();
+
+             // Check if the quiet field is present in the TOML table entry
+            let quiet = if let Some(quiet_value) = task_value.get("quiet") {
+                // If the quiet field is present, attempt to parse its value as a boolean
+                quiet_value
+                    .as_bool()
+                    .expect("Invalid quiet value. Expected boolean.")
+            } else {
+                // If the quiet field is not present, default to false
+                false
+            };
+
+            // Build task and add to hashmap
+            let task = Task { command, quiet };
+            tasks.insert(task_name.to_string(), task);
+        }
+    }
+
+    tasks
 }
