b3b8a687039360d7ddac97d142f61fe885e301ca
Josh Burns <joshyburnss@gmail.com>
2023-06-16 12:52:02 +1000

    add .execute() for Task

--- src/task.rs
+++ src/task.rs
@@ -3,3 +3,18 @@ pub struct Task {
     pub command: String,
     pub quiet: bool,
 }
+
+impl Task {
+    pub fn execute(&self) {
+        let output = Command::new("/bin/sh")
+            .arg("-c")
+            .arg(&self.command)
+            .output()
+            .expect("Failed to execute command.");
+
+        if !self.quiet {
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        }
+    }
+}
