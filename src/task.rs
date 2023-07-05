bcd0fb824db65b6c4cc6304277456662d4b718ca
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-07-05 15:15:22 +1000

    implement error handling

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,6 @@
 use std::io;
-use std::fs;
 use std::io::Write;
-use std::collections::HashMap;
-use std::process::{Command};
+use std::process::Command;
 
 use serde::Deserialize;
 
@@ -15,35 +13,35 @@ pub struct Task {
 }
 
 impl Task {
-    pub fn execute(&self) {
+    pub fn execute(&self) -> io::Result<()> {
         let output = Command::new("/bin/sh")
             .arg("-c")
             .arg(&self.command)
-            .output()
-            .expect("Failed to execute command.");
+            .output()?;
 
         if !self.quiet {
-            io::stdout().write_all(&output.stdout).unwrap();
-            io::stderr().write_all(&output.stderr).unwrap();
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
         }
+
+        Ok(())
     }
 
     pub fn check_targets(&self) {
-            if let Some(targets) = &self.targets {
-                for target in targets {
-                    println!("Checking target: {}", target);
-                }
+        if let Some(targets) = &self.targets {
+            for target in targets {
+                println!("Checking target: {}", target);
             }
-
+        }
     }
 }
 
-pub fn parse_taskfile(file_path: &str) -> HashMap<String, Task> {
-    let toml_content = fs::read_to_string(file_path).expect("Failed to read file.");
+pub fn parse_taskfile(file_path: &str) -> Result<HashMap<String, Task>, io::Error> {
+    let toml_content = fs::read_to_string(file_path)?;
 
-    let parsed_toml: toml::Value = toml::from_str(&toml_content).expect("Failed to parse TOML.");
+    let parsed_toml: toml::Value = toml::from_str(&toml_content)?;
 
-    extract_tasks(&parsed_toml)
+    Ok(extract_tasks(&parsed_toml))
 }
 
 fn extract_tasks(parsed_toml: &toml::Value) -> HashMap<String, Task> {
@@ -51,40 +49,53 @@ fn extract_tasks(parsed_toml: &toml::Value) -> HashMap<String, Task> {
 
     if let toml::Value::Table(table) = parsed_toml {
         for (task_name, task_value) in table {
-            // Parse command to run
-            let command = task_value["command"]
-                .as_str()
-                .expect("Invalid command.")
-                .to_string();
+            let command = match task_value.get("command") {
+                Some(value) => match value.as_str() {
+                    Some(cmd) => cmd.to_string(),
+                    None => {
+                        eprintln!("Invalid command for task '{}'", task_name);
+                        continue;
+                    }
+                },
+                None => {
+                    eprintln!("Missing command for task '{}'", task_name);
+                    continue;
+                }
+            };
 
-            // Parse the quiet flag
-            let quiet = task_value.get("quiet")
+            let quiet = task_value
+                .get("quiet")
                 .and_then(|quiet_value| quiet_value.as_bool())
                 .unwrap_or(false);
 
-            // Parse dependency tasks
-            let dependencies = task_value.get("dependencies")
+            let dependencies = task_value
+                .get("dependencies")
                 .and_then(|dependencies_value| dependencies_value.as_array())
-                .map(|dependencies|{
+                .map(|dependencies| {
                     dependencies
-                    .iter()
-                    .filter_map(|dependency_value| dependency_value.as_str())
-                    .map(|dependency| dependency.to_string())
-                    .collect::<Vec<String>>()
+                        .iter()
+                        .filter_map(|dependency_value| dependency_value.as_str())
+                        .map(|dependency| dependency.to_string())
+                        .collect::<Vec<String>>()
                 });
 
-            // Parse dependency tasks
-            let targets = task_value.get("targets")
+            let targets = task_value
+                .get("targets")
                 .and_then(|targets_value| targets_value.as_array())
-                .map(|targets|{
+                .map(|targets| {
                     targets
-                    .iter()
-                    .filter_map(|target_value| target_value.as_str())
-                    .map(|target| target.to_string())
-                    .collect::<Vec<String>>()
+                        .iter()
+                        .filter_map(|target_value| target_value.as_str())
+                        .map(|target| target.to_string())
+                        .collect::<Vec<String>>()
                 });
 
-            let task = Task { command, quiet, dependencies, targets };
+            let task = Task {
+                command,
+                quiet,
+                dependencies,
+                targets,
+            };
             tasks.insert(task_name.to_string(), task);
         }
     }
